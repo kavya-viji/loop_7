32bff1341f4a85ba1f301a27e43c9c9e4a46a268
Admin <kavyak6999@gmail.com>
2022-12-14 14:39:28 +0530

    switch

---
+++ loop/loop6.js
@@ -0,0 +1,22 @@
+var grade = prompt("Enter the grade");
+switch(grade){
+
+case "s":
+alert ("Super Grade");
+break;
+
+case "a":
+alert ("excellent Grade");
+break;
+
+case "e":
+alert ("just pass");
+break;
+
+case "u":
+alert ("failed Grade");
+break;
+
+default:
+alert ("unknown grade")
+}
